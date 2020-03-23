8336474b12c87d88458f1293b03abc52c7ec1d4e
Zcompzhe <314516682@qq.com>
2020-03-23 11:25:40 +0800

    dev

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,9 +1,10 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
+var merge = require("webpack-merge");
+var prodEnv = require("./prod.env");
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-   BASE_URL: '"http://139.224.25.25:8080"',
+  BASE_URL: '"http://139.224.25.25:8080"',
+  USER_URL: '"http://139.224.25.25:8001"'
   // BASE_URL: '"http://127.0.0.1:8080"',
-  USER_URL: '"http://127.0.0.1:8001"',
-})
+  // USER_URL: '"http://127.0.0.1:8001"',
+});
