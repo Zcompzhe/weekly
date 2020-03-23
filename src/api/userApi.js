97b78a97b55822be4ebbea382d3f5a47f35f1cd8
Zcompzhe <314516682@qq.com>
2020-03-23 15:28:55 +0800

    fix

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -5,31 +5,31 @@ import * as cascader from "./dataChange";
 
 //根据条件更新个人信息
 export const updateSGCCStaff = list => {
-  return request.post(`/apis/userManagement/updateSGCCStaff`, list);
+  return request.post(`/userManagement/updateSGCCStaff`, list);
 };
 
 //根据条件搜索用户名
 export const findUsername = list => {
-  return request.post(`/apis/userManagement/findUsername`, list);
+  return request.post(`/userManagement/findUsername`, list);
 };
 //根据条件搜索人员
 export const getStaffInfoByCondition = list => {
-  return request.post(`/apis/userManagement/getStaffInfoByCondition`, list);
+  return request.post(`/userManagement/getStaffInfoByCondition`, list);
 };
 
 //获取所有部门信息
 export const getAllSGCCDept = () => {
-  return request.get(`/apis/userManagement/getAllSGCCDept`);
+  return request.get(`/userManagement/getAllSGCCDept`);
 };
 
 //获取所有职位信息
 export const getAllSGCCPosition = () => {
-  return request.get(`/apis/userManagement/getAllSGCCPosition`);
+  return request.get(`/userManagement/getAllSGCCPosition`);
 };
 
 //根据id删除人员
 export const deleteSGCCStaffById = id => {
-  return request.delete(`/apis/userManagement/deleteSGCCStaffById`, {
+  return request.delete(`/userManagement/deleteSGCCStaffById`, {
     params: {
       id: id
     }
@@ -37,12 +37,12 @@ export const deleteSGCCStaffById = id => {
 };
 //根据条件添加个人信息
 export const addSGCCStaff = list => {
-  return request.post(`/apis/userManagement/addSGCCStaff`, list);
+  return request.post(`/userManagement/addSGCCStaff`, list);
 };
 
 //根据id删除人员
 export const initUserPassById = id => {
-  return request.get(`/apis/userManagement/initUserPassById`, {
+  return request.get(`/userManagement/initUserPassById`, {
     params: {
       id: id
     }
